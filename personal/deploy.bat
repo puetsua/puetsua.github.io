3bed5aed6d8ac2ed0989dd447ae039d5b91f107a
Pue-Tsuâ <puetsua@gmail.com>
2019-07-24 09:50:46 +0800

    Added deploy script.

---
+++ personal/deploy.bat
@@ -0,0 +1,17 @@
+echo "Clearing /dist...""
+rmdir /s /q dist
+
+echo "Building..."
+call npm run build
+
+echo "Creating git repository..."
+cd dist
+git init
+git add -A
+git commit -m "Deploy new version."
+
+echo "Uploading... "
+git push -f git@github.com:puetsua/puetsua.github.io.git master
+cd ..
+
+echo "Completed!"
