1e92f3e1d44fd9c315c7d79f60bc9d0dcf014c7c
Pue-Tsuâ <puetsua@gmail.com>
2020-08-06 00:34:24 +0800

    Updated TOS

--- pages/index/art/tos.vue
+++ pages/index/art/tos.vue
@@ -3,12 +3,12 @@ en:
   title: Terms of Service | Pue-Tsuâ
   description: Puetsua Art Terms of Service.
   tosTitle: Terms of Service (Puetsua Art)
-  tosUpdate: "Last Updated: September 29th, 2019"
+  tosUpdate: "Last Updated: August 6th, 2020"
 zh:
   title: 服務條款 | 飛蛇
   description: 繪圖委託的服務條款
   tosTitle: 服務條款 (飛蛇的繪圖委託) (待翻譯...)
-  tosUpdate: "上次更新：民國108年(2019) 9月29日"
+  tosUpdate: "上次更新：民國109年(2020) 8月6日"
 </i18n>
 
 <template>
@@ -21,28 +21,17 @@ zh:
     </p>
 
     <h3>About the commission</h3>
-    <p>
-      The art you commissioned is only for personal use, such as using it as your avatar, banner or personal website etc. Commercial use without notifying me first is NOT allowed.
-    </p>
-
-    <p>
-      <b>For Point Commissions, please check <a href="https://www.deviantart.com/puetsua/shop/commissions" target="_blank">my DeviantArt commission widget</a> on my page.</b> Without any specific points on the list, the convert rate at
-      <img alt="DA points"  src="https://e.deviantart.net/emoticons/p/points.gif"/>100 = $1
-    </p>
+    <p>The art you commissioned is only for personal use, such as using it as your avatar, banner or personal website etc. Commercial use without notifying me first is NOT allowed.</p>
 
     <h3>Before you commission</h3>
-    <p>
-      To commission, the reference to your characters must be included in your message. Any images of your character are fine. You cannot just say "I want my OC drawn" because I cannot read your mind. Other details like poses and expression are also recommended to be included.
-    </p>
+    <p>To commission, the reference to your characters must be included in your message. Any images of your character are fine. You cannot just say "I want my OC drawn" because I cannot read your mind. Other details like poses and expression are also recommended to be included.</p>
 
     <p>
-      You need to pay via
-      <b>Paypal or DeviantArt points</b>. I only start drawing your commission when I received the payment.
+      Paying via
+      <b>Paypal</b> is recommended. I only start drawing your commission when I received the payment.
     </p>
 
-    <p>
-      Contact me via DeviantArt Note, Twitter DM or Facebook Message about your commission. Once I accepted your commission we can talk about details from other social media, such as Discord, if you think it's more convenient.
-    </p>
+    <p>Contact me via Discord, Twitter DM or any social media about your commission.</p>
 
     <p>
       <b>The art can take one month to finish</b> since I have a project to do. If you want it to be done in a certain time, we can discuss.
@@ -51,13 +40,9 @@ zh:
     <p>If you don't want your commissioned artwork to be public please let me know in the note. Your name will be hidden and replaced with (Anon XXXXXX) in my to-do list. XXXXXX is 6-alphanumeric code I will give you once I received the payment.</p>
 
     <h3>During my work</h3>
-    <p>
-      I always send a sketch you before I do linework and coloring, if you found something isn't right, try to point them out here as many as possible.
-    </p>
+    <p>I always send a sketch you before I do linework and coloring, if you found something isn't right, try to point them out during sketch stage as many as possible. Any modification after sketch stage will cost you more.</p>
 
-    <p>
-      If you would like to receive other WIP images, such as linework or flat-colored. Just ask away.
-    </p>
+    <p>If you would like to receive other WIP images, such as linework or flat-colored. Just ask away.</p>
   </div>
 </template>
 
