e116aefafbc423e872a20e94debc09e293356f49
Pue-Tsuâ <puetsua@protonmail.com>
2021-05-26 21:29:29 +0800

    Changed mandarin about me description.

--- pages/index/about.vue
+++ pages/index/about.vue
@@ -29,31 +29,49 @@ zh:
   descDegree: |
     臺灣國立海洋大學資訊工程學系碩士畢業。曾經在網路工程相關的公司上班三年。
     是位的業餘繪師，有時候會接一些委託賺小錢。
-    目前是遊戲設計師，專注於遊戲開發。
+    目前專職遊戲開發，Unity工程師，Godot新手，正在學習遊戲設計。
   descKnowLang: |
     我會國語(普通話)，英文跟台灣閩南語。目前正在學日文做為第四語言。
 </i18n>
 
 <template>
   <div id="about">
-    <div style="height:50px" />
-    <center><img src="@/assets/aboutme.png"></center>
-    <h2>{{ $t('aboutme') }}</h2>
-    <p>{{ $t('descMyRealName') }}</p>
-    <p>{{ $t('descMyName') }}</p>
-    <p>{{ $t('descDegree') }}</p>
-    <p>{{ $t('descKnowLang') }}</p>
-    <h2>{{ $t('supportme') }}</h2>
-    <a href="https://ko-fi.com/puetsua"><img src="@/assets/BuyMeACoffee_Red@2x.png" height="50px"></a>
-    <p>{{ $t('supportdesc') }}</p>
-    <h2>{{ $t('contactme') }}</h2>
-    <a href="https://twitter.com/puetsua" target="_blank"><img src="@/assets/socialmedia_twitter.png" height="50px"></a>
-    <a href="https://www.deviantart.com/puetsua" target="_blank"><img src="@/assets/socialmedia_da.png" height="50px"></a>
-    <a href="https://www.facebook.com/puetsua" target="_blank"><img src="@/assets/socialmedia_fb.png" height="50px"></a>
-    <a href="https://www.instagram.com/puetsua.art/" target="_blank"><img src="@/assets/socialmedia_instagram.png" height="50px" /></a>
-    <a href="https://www.youtube.com/channel/UCtAVtsEW_OHh4X7yWjOVoLQ" target="_blank"><img src="@/assets/socialmedia_yt.png" height="50px"></a>
-    <a href="https://puetsua.itch.io/" target="_blank"><img src="@/assets/socialmedia_itchio.png" height="50px"></a>
-    <a href="mailto:puetsua@gmail.com"><img src="@/assets/socialmedia_email.png" height="50px"></a>
+    <div style="height: 50px" />
+    <center><img src="@/assets/aboutme.png" /></center>
+    <h2>{{ $t("aboutme") }}</h2>
+    <p>{{ $t("descMyRealName") }}</p>
+    <p>{{ $t("descMyName") }}</p>
+    <p>{{ $t("descDegree") }}</p>
+    <p>{{ $t("descKnowLang") }}</p>
+    <h2>{{ $t("supportme") }}</h2>
+    <a href="https://ko-fi.com/puetsua"
+      ><img src="@/assets/BuyMeACoffee_Red@2x.png" height="50px"
+    /></a>
+    <p>{{ $t("supportdesc") }}</p>
+    <h2>{{ $t("contactme") }}</h2>
+    <a href="https://twitter.com/puetsua" target="_blank"
+      ><img src="@/assets/socialmedia_twitter.png" height="50px"
+    /></a>
+    <a href="https://www.deviantart.com/puetsua" target="_blank"
+      ><img src="@/assets/socialmedia_da.png" height="50px"
+    /></a>
+    <a href="https://www.facebook.com/puetsua" target="_blank"
+      ><img src="@/assets/socialmedia_fb.png" height="50px"
+    /></a>
+    <a href="https://www.instagram.com/puetsua.art/" target="_blank"
+      ><img src="@/assets/socialmedia_instagram.png" height="50px"
+    /></a>
+    <a
+      href="https://www.youtube.com/channel/UCtAVtsEW_OHh4X7yWjOVoLQ"
+      target="_blank"
+      ><img src="@/assets/socialmedia_yt.png" height="50px"
+    /></a>
+    <a href="https://puetsua.itch.io/" target="_blank"
+      ><img src="@/assets/socialmedia_itchio.png" height="50px"
+    /></a>
+    <a href="mailto:puetsua@gmail.com"
+      ><img src="@/assets/socialmedia_email.png" height="50px"
+    /></a>
   </div>
 </template>
 
